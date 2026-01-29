3fd4633cbf8d49abccfb40039d12bbd7d667c29e
ch3p4ll3 <slinucs@gmail.com>
2026-01-29 16:59:30 +0100

    add enums

--- src/driver/ad1256.py
+++ src/driver/ad1256.py
@@ -1,69 +1,7 @@
 from src.settings import Settings
 
 from .spi_driver import SPIDriver
-from .enums import ScanMode
-
-
-
-# gain channel
-ADS1256_GAIN_E = {'ADS1256_GAIN_1' : 0, # GAIN   1
-                  'ADS1256_GAIN_2' : 1,	# GAIN   2
-                  'ADS1256_GAIN_4' : 2,	# GAIN   4
-                  'ADS1256_GAIN_8' : 3,	# GAIN   8
-                  'ADS1256_GAIN_16' : 4,# GAIN  16
-                  'ADS1256_GAIN_32' : 5,# GAIN  32
-                  'ADS1256_GAIN_64' : 6,# GAIN  64
-                 }
-
-# data rate
-ADS1256_DRATE_E = {'ADS1256_30000SPS' : 0xF0, # reset the default values
-                   'ADS1256_15000SPS' : 0xE0,
-                   'ADS1256_7500SPS' : 0xD0,
-                   'ADS1256_3750SPS' : 0xC0,
-                   'ADS1256_2000SPS' : 0xB0,
-                   'ADS1256_1000SPS' : 0xA1,
-                   'ADS1256_500SPS' : 0x92,
-                   'ADS1256_100SPS' : 0x82,
-                   'ADS1256_60SPS' : 0x72,
-                   'ADS1256_50SPS' : 0x63,
-                   'ADS1256_30SPS' : 0x53,
-                   'ADS1256_25SPS' : 0x43,
-                   'ADS1256_15SPS' : 0x33,
-                   'ADS1256_10SPS' : 0x20,
-                   'ADS1256_5SPS' : 0x13,
-                   'ADS1256_2d5SPS' : 0x03
-                  }
-
-# registration definition
-REG_E = {'REG_STATUS' : 0,  # x1H
-         'REG_MUX' : 1,     # 01H
-         'REG_ADCON' : 2,   # 20H
-         'REG_DRATE' : 3,   # F0H
-         'REG_IO' : 4,      # E0H
-         'REG_OFC0' : 5,    # xxH
-         'REG_OFC1' : 6,    # xxH
-         'REG_OFC2' : 7,    # xxH
-         'REG_FSC0' : 8,    # xxH
-         'REG_FSC1' : 9,    # xxH
-         'REG_FSC2' : 10,   # xxH
-        }
-
-# command definition
-CMD = {'CMD_WAKEUP' : 0x00,     # Completes SYNC and Exits Standby Mode 0000  0000 (00h)
-       'CMD_RDATA' : 0x01,      # Read Data 0000  0001 (01h)
-       'CMD_RDATAC' : 0x03,     # Read Data Continuously 0000   0011 (03h)
-       'CMD_SDATAC' : 0x0F,     # Stop Read Data Continuously 0000   1111 (0Fh)
-       'CMD_RREG' : 0x10,       # Read from REG rrr 0001 rrrr (1xh)
-       'CMD_WREG' : 0x50,       # Write to REG rrr 0101 rrrr (5xh)
-       'CMD_SELFCAL' : 0xF0,    # Offset and Gain Self-Calibration 1111    0000 (F0h)
-       'CMD_SELFOCAL' : 0xF1,   # Offset Self-Calibration 1111    0001 (F1h)
-       'CMD_SELFGCAL' : 0xF2,   # Gain Self-Calibration 1111    0010 (F2h)
-       'CMD_SYSOCAL' : 0xF3,    # System Offset Calibration 1111   0011 (F3h)
-       'CMD_SYSGCAL' : 0xF4,    # System Gain Calibration 1111    0100 (F4h)
-       'CMD_SYNC' : 0xFC,       # Synchronize the A/D Conversion 1111   1100 (FCh)
-       'CMD_STANDBY' : 0xFD,    # Begin Standby Mode 1111   1101 (FDh)
-       'CMD_RESET' : 0xFE,      # Reset to Power-Up Values 1111   1110 (FEh)
-      }
+from .enums import ScanMode, ADCGain, DataRate, Registry, Commands
 
 
 class ADS1256:
@@ -76,7 +14,7 @@ class ADS1256:
         self.spi = SPIDriver(settings)
 
         self.__init_adc()
-    
+
     def __init_adc(self):
         if self.spi.module_init() != 0:
             return -1
@@ -87,7 +25,7 @@ class ADS1256:
         else:
             print("ID Read failed   ")
             return -1
-        self.config_adc(ADS1256_GAIN_E['ADS1256_GAIN_1'], ADS1256_DRATE_E['ADS1256_30000SPS'])
+        self.config_adc(ADCGain.GAIN_1, DataRate.DR_30000SPS)
         return 0
 
     # Hardware reset
@@ -103,14 +41,14 @@ class ADS1256:
         self.spi.spi_writebyte([reg])
         self.spi.digital_write(self.cs_pin, 1)#cs 1
 
-    def write_reg(self, reg, data):
+    def write_reg(self, reg: Registry, data):
         self.spi.digital_write(self.cs_pin, 0)#cs  0
-        self.spi.spi_writebyte([CMD['CMD_WREG'] | reg, 0x00, data])
+        self.spi.spi_writebyte([Commands.CMD_WREG.value | reg.value, 0x00, data])
         self.spi.digital_write(self.cs_pin, 1)#cs 1
 
     def read_data(self, reg):
         self.spi.digital_write(self.cs_pin, 0)#cs  0
-        self.spi.spi_writebyte([CMD['CMD_RREG'] | reg, 0x00])
+        self.spi.spi_writebyte([Commands.CMD_RREG.value | reg, 0x00])
         data = self.spi.spi_readbytes(1)
         self.spi.digital_write(self.cs_pin, 1)#cs 1
 
@@ -126,22 +64,22 @@ class ADS1256:
 
     def read_chip_id(self):
         self.wait_drdy()
-        id = self.read_data(REG_E['REG_STATUS'])
+        id = self.read_data(Registry.REG_STATUS)
         id = id[0] >> 4
         # print 'ID',id
         return id
 
     #The configuration parameters of ADC, gain and data rate
-    def config_adc(self, gain, drate):
+    def config_adc(self, gain: ADCGain, drate: DataRate):
         self.wait_drdy()
         buf = [0,0,0,0,0,0,0,0]
         buf[0] = (0<<3) | (1<<2) | (0<<1)
         buf[1] = 0x08
-        buf[2] = (0<<5) | (0<<3) | (gain<<0)
-        buf[3] = drate
+        buf[2] = (0<<5) | (0<<3) | (gain.value<<0)
+        buf[3] = drate.value
 
         self.spi.digital_write(self.cs_pin, 0)#cs  0
-        self.spi.spi_writebyte([CMD['CMD_WREG'] | 0, 0x03])
+        self.spi.spi_writebyte([Commands.CMD_WREG.value | 0, 0x03])
         self.spi.spi_writebyte(buf)
 
         self.spi.digital_write(self.cs_pin, 1)#cs 1
@@ -150,17 +88,17 @@ class ADS1256:
     def set_channel(self, channel):
         if channel > 7:
             return 0
-        self.write_reg(REG_E['REG_MUX'], (channel<<4) | (1<<3))
+        self.write_reg(Registry.REG_MUX, (channel<<4) | (1<<3))
 
     def set_differential_channel(self, channel):
         if channel == 0:
-            self.write_reg(REG_E['REG_MUX'], (0 << 4) | 1) 	#Diffchannel  AIN0-AIN1
+            self.write_reg(Registry.REG_MUX, (0 << 4) | 1) 	#Diffchannel  AIN0-AIN1
         elif channel == 1:
-            self.write_reg(REG_E['REG_MUX'], (2 << 4) | 3) 	#Diffchannel   AIN2-AIN3
+            self.write_reg(Registry.REG_MUX, (2 << 4) | 3) 	#Diffchannel   AIN2-AIN3
         elif channel == 2:
-            self.write_reg(REG_E['REG_MUX'], (4 << 4) | 5) 	#Diffchannel    AIN4-AIN5
+            self.write_reg(Registry.REG_MUX, (4 << 4) | 5) 	#Diffchannel    AIN4-AIN5
         elif channel == 3:
-            self.write_reg(REG_E['REG_MUX'], (6 << 4) | 7) 	#Diffchannel   AIN6-AIN7
+            self.write_reg(Registry.REG_MUX, (6 << 4) | 7) 	#Diffchannel   AIN6-AIN7
 
     def set_mode(self, mode: ScanMode):
         self.scan_mode = mode
@@ -168,7 +106,7 @@ class ADS1256:
     def read_ADC_data(self):
         self.wait_drdy()
         self.spi.digital_write(self.cs_pin, 0)#cs  0
-        self.spi.spi_writebyte([CMD['CMD_RDATA']])
+        self.spi.spi_writebyte([Commands.CMD_RDATA.value])
         # config.delay_ms(10)
 
         buf = self.spi.spi_readbytes(3)
@@ -185,18 +123,18 @@ class ADS1256:
             if channel >= 8:
                 return 0
             self.set_channel(channel)
-            self.write_cmd(CMD['CMD_SYNC'])
+            self.write_cmd(Commands.CMD_SYNC.value)
             # config.delay_ms(10)
-            self.write_cmd(CMD['CMD_WAKEUP'])
+            self.write_cmd(Commands.CMD_WAKEUP.value)
             # config.delay_ms(200)
             value = self.read_ADC_data()
         else:
             if channel>=4:
                 return 0
             self.set_differential_channel(channel)
-            self.write_cmd(CMD['CMD_SYNC'])
+            self.write_cmd(Commands.CMD_SYNC.value)
             # config.delay_ms(10) 
-            self.write_cmd(CMD['CMD_WAKEUP'])
+            self.write_cmd(Commands.CMD_WAKEUP.value)
             # config.delay_ms(10) 
             value = self.read_ADC_data()
         return value * 5.0 / 0x7fffff
--- src/driver/enums.py
+++ src/driver/enums.py
@@ -5,3 +5,63 @@ from enum import IntEnum
 class ScanMode(IntEnum):
     SingleMode = 0
     DifferentialMode = 1
+
+
+class ADCGain(IntEnum):
+    GAIN_1 = 0, # GAIN   1
+    GAIN_2 = 1,	# GAIN   2
+    GAIN_4 = 2,	# GAIN   4
+    GAIN_8 = 3,	# GAIN   8
+    GAIN_16 = 4,# GAIN  16
+    GAIN_32 = 5,# GAIN  32
+    GAIN_64 = 6,# GAIN  64
+
+
+class DataRate(IntEnum):
+    DR_30000SPS = 0xF0,
+    DR_15000SPS = 0xE0,
+    DR_7500SPS = 0xD0,
+    DR_3750SPS = 0xC0,
+    DR_2000SPS = 0xB0,
+    DR_1000SPS = 0xA1,
+    DR_500SPS = 0x92,
+    DR_100SPS = 0x82,
+    DR_60SPS = 0x72,
+    DR_50SPS = 0x63,
+    DR_30SPS = 0x53,
+    DR_25SPS = 0x43,
+    DR_15SPS = 0x33,
+    DR_10SPS = 0x20,
+    DR_5SPS = 0x13,
+    DR_2d5SPS = 0x03
+
+
+class Registry(IntEnum):
+    REG_STATUS = 0,  # x1H
+    REG_MUX = 1,     # 01H
+    REG_ADCON = 2,   # 20H
+    REG_DRATE = 3,   # F0H
+    REG_IO = 4,      # E0H
+    REG_OFC0 = 5,    # xxH
+    REG_OFC1 = 6,    # xxH
+    REG_OFC2 = 7,    # xxH
+    REG_FSC0 = 8,    # xxH
+    REG_FSC1 = 9,    # xxH
+    REG_FSC2 = 10,   # xxH
+
+
+class Commands(IntEnum):
+    CMD_WAKEUP = 0x00,     # Completes SYNC and Exits Standby Mode 0000  0000 (00h)
+    CMD_RDATA = 0x01,      # Read Data 0000  0001 (01h)
+    CMD_RDATAC = 0x03,     # Read Data Continuously 0000   0011 (03h)
+    CMD_SDATAC = 0x0F,     # Stop Read Data Continuously 0000   1111 (0Fh)
+    CMD_RREG = 0x10,       # Read from REG rrr 0001 rrrr (1xh)
+    CMD_WREG = 0x50,       # Write to REG rrr 0101 rrrr (5xh)
+    CMD_SELFCAL = 0xF0,    # Offset and Gain Self-Calibration 1111    0000 (F0h)
+    CMD_SELFOCAL = 0xF1,   # Offset Self-Calibration 1111    0001 (F1h)
+    CMD_SELFGCAL = 0xF2,   # Gain Self-Calibration 1111    0010 (F2h)
+    CMD_SYSOCAL = 0xF3,    # System Offset Calibration 1111   0011 (F3h)
+    CMD_SYSGCAL = 0xF4,    # System Gain Calibration 1111    0100 (F4h)
+    CMD_SYNC = 0xFC,       # Synchronize the A/D Conversion 1111   1100 (FCh)
+    CMD_STANDBY = 0xFD,    # Begin Standby Mode 1111   1101 (FDh)
+    CMD_RESET = 0xFE,      # Reset to Power-Up Values 1111   1110 (FEh)
